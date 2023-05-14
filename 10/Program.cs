e57dcba8dee4545c52960949c7fb581c63e8d470
Aika <akia2710@gmail.com>
2023-05-14 07:29:18 +0300

    дз2

---
+++ 10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int NewNumber(int N)
+{
+    int digit1 = N / 100;
+    int digit3 = N % 10;
+    int result = digit1 * 10 + digit3;
+
+    return result;
+}
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное числр: {number}");
+int result = NewNumber(number);
+
+Console.WriteLine(result);
